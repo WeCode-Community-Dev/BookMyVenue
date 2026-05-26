292393c69ee7806cd508a09fad1adbb6b5f40132
nafanpe <nifunafannafpe@gmail.com>
2026-05-26 21:08:12 +0530

    feat: search bar and filters

---
+++ FRONTEND/src/components/Filters.jsx
@@ -0,0 +1,39 @@
+import { MapPin } from "@mynaui/icons-react";
+import { Kanban } from "@mynaui/icons-react";
+import { Dollar } from "@mynaui/icons-react";
+import { Speaker } from "@mynaui/icons-react";
+import { ChevronDown } from "@mynaui/icons-react";
+import { Config } from "@mynaui/icons-react";
+
+function Filters() {
+
+    const dropdownFilters = [
+        { name: "Location", icon: <MapPin size={20}/>},
+        { name: "Venue Type", icon: <Kanban size={20}/>},
+        { name: "Budget", icon: <Dollar size={20}/>},
+        { name: "Amenities", icon: <Speaker size={20}/>}
+    ]
+
+    return (
+        <div className="FILTERS flex flex-wrap items-center gap-3 pt-3">
+            {dropdownFilters.map((filter, index) => (
+                <button 
+                    key={index}
+                    className="flex items-center gap-2 px-4 py-2 bg-[#f9f9f7] text-[#2a5660] text-sm rounded-full shadow-md hover:bg-[#e0e0dc] hover:scale-105 active:scale-95 cursor-pointer transition-all duration-300 ease-in-out"
+                >
+                 <span>{filter.icon}</span>   
+                 <span>{filter.name}</span>
+                 <ChevronDown/>
+                </button>
+            ))}
+            {
+                <button className="flex items-center gap-2 px-4 py-2 bg-[#f56d5e] text-white text-sm rounded-full shadow-md hover:bg-[#BF5842] hover:scale-105 active:scale-95 cursor-pointer transition-all duration-300 ease-in-out">
+                    <Config/>
+                    <span>All Filters</span>
+                </button>
+            }
+        </div>
+    )
+}
+
+export default Filters
--- FRONTEND/src/pages/HomePage.jsx
+++ FRONTEND/src/pages/HomePage.jsx
@@ -1,12 +1,26 @@
-
+import Filters from "../components/Filters"
 
 function HomePage() {
+
+    const HeroContent = (
+        <div className="Hero-Content text-center text-[#2a5660]">
+            <h1 className="text-4xl md:text-3xl font-bold mb-4">Find the Perfect Venue for Your Event.</h1>
+            <p className="text-lg md:text-2xl mb-8">Discover and book unique spaces for your next gathering, party, or meeting.</p>
+        </div>
+    )
+
+    const Searchbar = (
+        <div className="SEARCH-BAR flex w-full max-w-2xl mx-auto mt-4">
+            <input type="text" placeholder="Search for venues and Venue Types..." className="w-full h-16 shadow-xl text-center text-lg px-4 py-3 rounded-l-full border border-gray-300 focus:outline-none focus:ring-2 focus:ring-[#AEAC9B] focus:border-transparent" />
+            <button className="px-6 py-3 bg-[#f56d5e] text-white text-base rounded-r-full shadow-xl hover:bg-[#BF5842] hover:scale-105 active:scale-95 cursor-pointer transition-all duration-300 ease-in-out">Search</button>
+        </div>
+    )
+
     return (
-        <div className="Hero-Section w-full h-screen bg-[#f4f4f2] bg-cover bg-center flex items-center justify-center">
-            <div className="Hero-Content text-center text-[#2a5660]">
-                <h1 className="text-4xl md:text-3xl font-bold mb-4">Find the Perfect Venue for Your Event.</h1>
-                <p className="text-lg md:text-2xl mb-8">Discover and book unique spaces for your next gathering, party, or meeting.</p>
-            </div>
+        <div className="Hero-Section flex flex-col w-full h-screen bg-[#f4f4f2] bg-cover bg-center flex items-center justify-center">
+            {HeroContent}
+            {Searchbar}
+            <Filters/>
         </div>
     )
 }
