dd872ae028a33f1c47aec0317e3d090bccb5c740
abhijithbg2001 <abhijithbg2k18@gmail.com>
2026-05-26 23:01:19 +0530

    SpringBoot rendered and intial db setup using docker

---
+++ Server/src/test/java/com/example/bookMyVenue/BookMyVenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookMyVenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyVenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
