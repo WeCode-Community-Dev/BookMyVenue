997f62739ffc32781ffceb43a6030aa100700fc0
Josephalexander06 <josephalexander283@gmailcom>
2026-05-31 13:56:54 +0530

    feat: update .gitignore and add requirements.txt for backend dependencies

--- backend/main.py
+++ backend/main.py
@@ -21,9 +21,9 @@ router = APIRouter(
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
 
 @app.post("/",status_code=status.HTTP_201_CREATED,response_model=List[CreateVenue])
 async def create_venue(Venues:CreateVenue,db:Session=Depends(get_db)):
@@ -34,10 +34,18 @@ async def create_venue(Venues:CreateVenue,db:Session=Depends(get_db)):
     
     return [new_venue]
 
-@app.get("/venues")
-async def get_venue():
-    return {"venues":[]}
+@app.get("/",status_code=status.HTTP_200_OK,response_model=List[CreateVenue])
+async def get_venue(db:Session=Depends(get_db)):
+    venues  = db.query(models.Venue).all()
+    return venues
 
-@app.get("/venues/{venue_id}")
-async def get_venues(venue_id:int):
-    return venue_id
+@app.get("/{id}",status_code=status.HTTP_200_OK,response_model=CreateVenue)
+async def get_venues(id:int,db: Session = Depends(get_db)):
+    # print(id)
+
+    venue = db.query(models.Venue).filter(models.Venue.id == id).first()
+    if not venue:
+        raise HTTPException(status_code=404,detail="Venue Not Found")
+    
+    return venue
+
