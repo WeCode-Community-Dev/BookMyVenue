da3aec4ab7fe6f4bb90ca0127dd7d3a9f903a80f
vishnuvvv <vishnusathyanathan@gmail.com>
2026-06-01 20:32:33 +0530

    implements the file and folder structure

---
+++ apps/frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div style={{ color: 'black' }}>Hellooo vishnu you are gonna get greate job well done</div>
+  );
+}
---
+++ apps/frontend/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>login or registration</h1>
+    </div>
+  )
+}
+
+export default page
