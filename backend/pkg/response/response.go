679008b198db4d6706afe0a05b8655515fa7cb0b
adeel <adeelpv360@gmail.com>
2026-06-03 12:51:45 +0530

    refactor(response): standardize API response structure

--- backend/pkg/response/response.go
+++ backend/pkg/response/response.go
@@ -2,17 +2,27 @@ package response
 
 import "github.com/gin-gonic/gin"
 
+type Response struct {
+	Sucess  bool   `json:"success"`
+	Message string `json:"message"`
+	Data    any    `json:"data,omitempty"`
+	Error   string `json:"error,omitempty"`
+}
+
 // Success sends a standardized JSON success response containing
 // a message and optional data payload with the specified HTTP status code.
-func Success(c *gin.Context, status int, message string, data any) {
-	c.JSON(status, gin.H{
-		"message": message,
-		"data":    data,
+func Success(c *gin.Context, statusCode int, message string, data any) {
+	c.JSON(statusCode, Response{
+		Sucess:  true,
+		Message: message,
+		Data:    data,
 	})
 }
 
-func Error(c *gin.Context, status int, message string) {
-	c.JSON(status, gin.H{
-		"Error": message,
+func Error(c *gin.Context, statusCode int, errMessage string) {
+	c.JSON(statusCode, Response{
+		Sucess:  false,
+		Message: errMessage,
+		Error:   errMessage,
 	})
 }
