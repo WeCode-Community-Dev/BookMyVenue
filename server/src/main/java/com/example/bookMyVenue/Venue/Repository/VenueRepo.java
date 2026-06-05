8eb74897992aa9562df6adbcf096d0bf25b3d033
abhijithbg2001 <abhijithbg2k18@gmail.com>
2026-06-05 11:53:22 +0530

    github account fix test

--- server/src/main/java/com/example/bookMyVenue/Venue/Repository/VenueRepo.java
+++ server/src/main/java/com/example/bookMyVenue/Venue/Repository/VenueRepo.java
@@ -1,9 +1,22 @@
 package com.example.bookMyVenue.Venue.Repository;
 
+import com.example.bookMyVenue.Enums.VenueActiveStatus;
+import com.example.bookMyVenue.Enums.VenueVerificationStatus;
 import com.example.bookMyVenue.Venue.Model.Venue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VenueRepo extends JpaRepository<Venue,Long> {
+    public List<Venue> findAllByVenueVerificationStatusAndVenueActivationStatus(
+            VenueVerificationStatus verificationStatus,
+            VenueActiveStatus venueActiveStatus );
+
+    public Venue findByIdAndVenueVerificationStatusAndVenueActivationStatus(
+            Long id,
+            VenueVerificationStatus verificationStatus,
+            VenueActiveStatus venueActiveStatus
+    );
 }
