418316efb7292e60616775edde58d095e0a733b7
Masmoora EK <masmooram@gmail.com>
2026-06-13 02:07:02 +0530

    feat(admin-user): update user model

--- server/src/infrastructure/database/models/User.model.js
+++ server/src/infrastructure/database/models/User.model.js
@@ -1,43 +1,47 @@
 import mongoose from "mongoose";
 import UserRole from "../../../domain/enums/UserRole.enum.js";
 const userSchema = new mongoose.Schema({
-    fullName:{
+    fullName: {
         type: String,
-        required:true
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    phone:{
-        type:String,
-        required:true,
-        unique:true
+    phone: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
-        select:false
+    password: {
+        type: String,
+        required: true,
+        select: false
     },
-    role:{
-        type:String,
+    role: {
+        type: String,
         enum: Object.values(UserRole),
         default: UserRole.CUSTOMER
     },
-    isDeleted: {
+    isOtpVerified: {
+        type: Boolean,
+        default: false
+    },
+    isBlocked: {
         type: Boolean,
         default: false
     },
     refreshToken: {
-    type: [String],
-    default: [],
-    select: false
-}
-    
+        type: [String],
+        default: [],
+        select: false
+    }
+
 },
     {
-        timestamps:true
+        timestamps: true
     })
 
 
