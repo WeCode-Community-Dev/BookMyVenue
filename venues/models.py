50808d026b68e4f867b1374378e064ead5e70aa2
shijas <jas8848918@gmail.com>
2026-06-16 17:37:51 +0530

    add service, facility, and price models for venue management

--- venues/models.py
+++ venues/models.py
@@ -3,31 +3,56 @@ from account.models import User
 from locations.models import City
 
 # Create your models here.
+class Venue(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    City = models.ForeignKey(City,on_delete=models.CASCADE)
+    name = models.CharField(max_length=225)
+    address = models.TextField()
+    # price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+   
+
+class Image(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='images')
+    venue_image = models.ImageField(upload_to='media/')
+
+
+class Service(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='services')
+    name = models.CharField(max_length=225)
+    capacity = models.IntegerField()
+    choice = models.BooleanField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.name
 
 class Facility(models.Model):
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, related_name='facilities')
     name = models.CharField(max_length=225)
+    choice = models.BooleanField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2,null=True,blank=True)
+
 
     def __str__(self):
         return self.name
-class Venue(models.Model):
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
-    city = models.ForeignKey(City, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    address = models.TextField()
-    description = models.TextField()
-    capacity = models.IntegerField()
-    price_per_hour = models.DecimalField(max_digits=10, decimal_places=2)
-    facilities = models.ManyToManyField(Facility)
 
+class Price(models.Model):
+    venue = models.ForeignKey(Venue,on_delete=models.CASCADE,related_name='prices')
+    price_verision = models.CharField(max_length=225)
+    amount = models.DecimalField(max_digits=10, decimal_places=5)
+    choice = models.BooleanField()
+
+     
     def __str__(self):
-        return self.name
+        return f"{self.price_verision}-{self.venue.name} - ₹{self.amount}"
+
+   
+
+
+
+
     
-class VenueImage(models.Model):
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='venues/')
-
-    def __str__(self):
-        return self.venue.name
-
-
-
