998adbe13cbb28f0d676f6d169ce8a94918e507e
rizwanc018 <rizwanc018@gmail.com>
2026-06-18 11:44:09 +0530

    modified:   apps/client/src/app/page.tsx 	deleted:    packages/ui/src/button.tsx 	deleted:    packages/ui/src/card.tsx 	deleted:    packages/ui/src/code.tsx

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@bookmyvenue/ui/button";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button appName="BookMyVenue" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">Click me</Button>
     </div>
   );
 }
