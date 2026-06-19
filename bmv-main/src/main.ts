ab2048577f7bc29fc30352c85040b585b08b9b00
Amith Biju <amithbiju@ug.cusat.ac.in>
2026-06-19 13:39:20 +0530

    feat(auth): added sawgger api  documenation setup

--- bmv-main/src/main.ts
+++ bmv-main/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+    // Swagger setup
+    const config = new DocumentBuilder()
+    .setTitle('BookMyVenue API').setDescription('API documentation for BookMyVenue application').setVersion('1.0').addTag('bookmyvenue').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
