04f576a0cf3ae74a152e9186ef7554c2af89d504
adeel <adeelpv360@gmail.com>
2026-06-27 11:36:39 +0530

    feat(utils): add utility function to convert pgtype.Numeric to int64

--- backend/pkg/utils/pgtypeNumeric.go
+++ backend/pkg/utils/pgtypeNumeric.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"math"
 	"math/big"
 
 	"github.com/jackc/pgx/v5/pgtype"
@@ -11,5 +13,22 @@ func Int64ToNumeric(v int64) pgtype.Numeric {
 		Int:   big.NewInt(v),
 		Exp:   0,
 		Valid: true,
+	}
+}
+
+func NumericToInt64(n pgtype.Numeric) (int64, error) {
+	if !n.Valid {
+		return 0, errors.New("invalid numeric")
+	}
+
+	i := n.Int.Int64()
+
+	switch {
+	case n.Exp == 0:
+		return i, nil
+	case n.Exp > 0:
+		return i * int64(math.Pow10(int(n.Exp))), nil
+	default:
+		return i / int64(math.Pow10(int(-n.Exp))), nil
 	}
 }
