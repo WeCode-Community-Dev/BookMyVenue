61e1ea71d946051a2ee831a84b3f8c551645a0ca
geek-ashkar <ashkartpgeek@gmail.com>
2026-06-30 01:18:24 +0200

    Add authentication APIs with root admin login

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,16 @@
 import express from "express";
-import { registerCustomer } from "../controllers/authController.js";
+import {
+  registerCustomer,
+  registerOwner,
+  loginUser,
+  rootAdminLogin,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register/customer", registerCustomer);
+router.post("/register/owner", registerOwner);
+router.post("/login", loginUser);
+router.post("/root-admin/login", rootAdminLogin);
 
 export default router;
