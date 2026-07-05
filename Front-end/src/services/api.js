a478097e6a9937f7c1e9a7701f36686d2eee485f
Nikhilk70 <nikhilksiva70@gmail.com>
2026-07-05 22:36:41 +0530

    API Connection 05

--- Front-end/src/services/api.js
+++ Front-end/src/services/api.js
@@ -16,3 +16,75 @@ export const registerUser = async (userData) => {
 
   return responseData;
 };
+
+export const createVenue = async (venueData, token) => {
+  const response = await fetch(`${API_BASE_URL}/api/venues`, {
+    method: "POST",
+    headers: { 
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`   // JWT needed
+    },
+    body: JSON.stringify(venueData)
+  });
+
+  const responseData = await response.json();
+  console.log("Create venue response:", responseData);
+
+  if (!response.ok) {
+    throw new Error(responseData.errors?.[0] || responseData.message || "Failed to create venue");
+  }
+
+  return responseData;
+};
+
+
+export const getAllVenues = async (token) => {
+  const response = await fetch(`${API_BASE_URL}/api/venues`, {
+    headers: { "Authorization": `Bearer ${token}` }
+  });
+
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.errors?.[0] || responseData.message || "Failed to load venues");
+  }
+  return responseData;
+};
+
+export const getAdminAllVenues = async (token) => {
+  const response = await fetch(`${API_BASE_URL}/api/admin/requests`, {
+    headers: { "Authorization": `Bearer ${token}` }
+  });
+
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.errors?.[0] || responseData.message || "Failed to load venues");
+  }
+  return responseData;
+};
+
+export const getAllUsers = async (token) => {
+  const response = await fetch(`${API_BASE_URL}/api/admin/users`, {
+    headers: { "Authorization": `Bearer ${token}` }
+  });
+
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.errors?.[0] || responseData.message || "Failed to load venues");
+  }
+  return responseData;
+};
+
+export const approveRequest = async (token, venueId, remarks) => {
+  const response = await fetch(
+    `${API_BASE_URL}/api/admin/requests/${venueId}/approve?remarks=${encodeURIComponent(remarks)}`,
+    {
+      method: "PATCH",
+      headers: { "Authorization": `Bearer ${token}` }
+    }
+  );
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.errors?.[0] || responseData.message || "Approve failed");
+  }
+  return responseData;
+};
