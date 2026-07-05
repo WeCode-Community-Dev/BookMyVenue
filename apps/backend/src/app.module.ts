b762de02a8bb47731db398dd7a44d9bbec32c0e2
vishnuvvv <vishnusathyanathan@gmail.com>
2026-07-05 22:54:21 +0530

    feat:implements athentication using otp

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RedisModule, MailModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
