c4109194f925179082269c9325daec5b58373290
akhil1234A <akhilanwarm@gmail.com>
2026-07-06 23:25:13 +0530

    Fix: migration fix

--- apps/api/alembic/versions/867c2871ef67_remove_slots_table.py
+++ apps/api/alembic/versions/867c2871ef67_remove_slots_table.py
@@ -1,17 +1,18 @@
 """remove_slots_table
 
 Revision ID: 867c2871ef67
-Revises: 5134b9fb1f9e
+Revises: c7ff94364ee8
 Create Date: 2026-07-05 15:25:33.028368
 
 """
+
 from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 revision: str = '867c2871ef67'
-down_revision: Union[str, None] = '5134b9fb1f9e'
+down_revision: Union[str, None] = "c7ff94364ee8"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
