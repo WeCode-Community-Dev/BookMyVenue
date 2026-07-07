075de29332516a1715a12a329f15496cd3df27dc
sankar <sankusanku001@gmail.com>
2026-07-07 01:43:21 +0100

    feat: implement theme provider and navbar component with dark mode support

--- apps/user-web/src/main.tsx
+++ apps/user-web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from '@venue404/ui'
 import App from './App'
 import { AuthProvider } from './lib/AuthContext'
 import './styles/index.css'
@@ -8,10 +9,12 @@ import { queryClient } from './lib/queryClient'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryClientProvider>
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
