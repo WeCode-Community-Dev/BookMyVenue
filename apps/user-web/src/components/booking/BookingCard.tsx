3a2e072cc8cdbfbcf764294e2eae9b314c5ac636
sankar <sankusanku001@gmail.com>
2026-07-09 01:59:22 +0100

    fix: BookingCard duplicate button fix

--- apps/user-web/src/components/booking/BookingCard.tsx
+++ apps/user-web/src/components/booking/BookingCard.tsx
@@ -136,27 +136,22 @@ export default function MyBookingCard({ booking }: Props) {
           )}
 
           <div className="mt-auto flex items-center justify-end pt-8">
-            <div className='flex items-end justify-center gap-2'>
-              <Link to={`/bookings/${booking.id}`} className="mt-8">
-                <Button>View Booking</Button>
-              </Link>
-              <Button
-                onClick={() =>
-                  requiresInstantPayment
-                    ? navigate(`/payment/${booking.id}?type=full`)
-                    : requiresAdvance
-                      ? navigate(`/payment/${booking.id}?type=advance`)
-                      : requiresBalance
-                        ? navigate(`/payment/${booking.id}?type=balance`)
-                        : navigate(`/bookings/${booking.id}`)
-                }
-              >
-                {actionLabel}
-              </Button>
-            </div>
+            <Button
+              onClick={() =>
+                requiresInstantPayment
+                  ? navigate(`/payment/${booking.id}?type=full`)
+                  : requiresAdvance
+                    ? navigate(`/payment/${booking.id}?type=advance`)
+                    : requiresBalance
+                      ? navigate(`/payment/${booking.id}?type=balance`)
+                      : navigate(`/bookings/${booking.id}`)
+              }
+            >
+              {actionLabel}
+            </Button>
           </div>
         </div>
       </div>
-    </Card>
+    </Card >
   )
 }
