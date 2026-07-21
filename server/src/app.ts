38597f0edbaacdb144266e14d6ea3e839776d245
Ashkar <ashkar@Mac.fritz.box>
2026-07-22 00:29:28 +0200

    Fix macOS setup and update backend port to 5001

--- server/src/app.ts
+++ server/src/app.ts
@@ -44,6 +44,8 @@ app.use("/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+console.log("Starting server...");
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
